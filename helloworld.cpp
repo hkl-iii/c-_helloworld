d1ddb3b00f90ff90859a23e0b2827f5381af0a29
hkl-iii <rahleggon@gmail.com>
2021-07-15 21:41:16 -0500

    updated helloworldcpp, seeing error line 10,11 but running code still works??

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,12 @@
 //Preprocessor directive that includes header iostream
 #include <iostream>
+#include <string>
 
 
 //Start of your program: function block main()
 int main() {
 
-/*Write to the screen std::cout*/
+//Write to the screen std::cout 
     std::cout<<"Hello World!" <<std::endl;
     string fruit = "apple";
     string fruits[5]={"apple","orange","strawberry","banana"};
